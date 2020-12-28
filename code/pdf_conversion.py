38331eb47685f3d1fdcc5d45829438b1dac8dc82
trholdridge <trholdridge@gmail.com>
2020-12-27 20:54:15 -0800

    added pdf conversion file

---
+++ code/pdf_conversion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 19:20:16 2020
+
+@author: trhol
+"""
+from tika import parser
+
+# pdf_to_text
+# use path to pdf file, get raw text using tika
+#def pdf_to_text(path):
+    
